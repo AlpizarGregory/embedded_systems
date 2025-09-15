e058c353d5e01c4f5d03a14815d4ec5bb92205cb
Bryang2303 <bryang2303@gmail.com>
2025-09-15 15:34:16 -0600

    HUD Component creation
    Component HUD added and personalized (remote control of the car and status view)

--- project_1/front_end_car/src/App.jsx
+++ project_1/front_end_car/src/App.jsx
@@ -1,6 +1,7 @@
 import Start from './components/Start'
 import Login from './components/Login'
 import SignUp from './components/SignUp'
+import HUD from './components/HUD'
 import { Routes, Route } from "react-router-dom";
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/hud" element={<HUD />} />
     </Routes>
     
     
