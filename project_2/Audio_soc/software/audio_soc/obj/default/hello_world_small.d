4e3bbf7e0a754a36984259f931bc607918b7a08a
Bryang2303 <bryang2303@gmail.com>
2025-11-18 12:40:55 -0600

    Generation of a NiosII application from a BSP template
    A Nios II application generated from a "Hello world small" BSP template, to simulate the system

---
+++ project_2/Audio_soc/software/audio_soc/obj/default/hello_world_small.d
@@ -0,0 +1,9 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../audio_soc_bsp/system.h ../audio_soc_bsp/linker.h \
+ ../audio_soc_bsp//HAL/inc/sys/alt_stdio.h
+
+../audio_soc_bsp/system.h:
+
+../audio_soc_bsp/linker.h:
+
+../audio_soc_bsp//HAL/inc/sys/alt_stdio.h:
