38eb8e3a1d6ee2d5aa18b15ceae4d97de28a3c04
Jose Maria Vindas Ortiz <josemaria130@outlook.es>
2025-11-24 21:17:24 -0600

    Basic NIOS Plataform Design

---
+++ project_2/GPIO/Prueba_Taller_Test/BasicNIOS_bb.v
@@ -0,0 +1,10 @@
+
+module BasicNIOS (
+	clk_clk,
+	leds_output_export,
+	reset_reset_n);	
+
+	input		clk_clk;
+	output	[7:0]	leds_output_export;
+	input		reset_reset_n;
+endmodule
---
+++ project_2/GPIO/Prueba_Taller_Test/BasicNIOS_inst.vhd
@@ -0,0 +1,15 @@
+	component BasicNIOS is
+		port (
+			clk_clk            : in  std_logic                    := 'X'; -- clk
+			leds_output_export : out std_logic_vector(7 downto 0);        -- export
+			reset_reset_n      : in  std_logic                    := 'X'  -- reset_n
+		);
+	end component BasicNIOS;
+
+	u0 : component BasicNIOS
+		port map (
+			clk_clk            => CONNECTED_TO_clk_clk,            --         clk.clk
+			leds_output_export => CONNECTED_TO_leds_output_export, -- leds_output.export
+			reset_reset_n      => CONNECTED_TO_reset_reset_n       --       reset.reset_n
+		);
+
