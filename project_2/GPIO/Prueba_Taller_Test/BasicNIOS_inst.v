5fd5fdacc0ec414a13f120f63579f548a13f26a3
Jose Maria Vindas Ortiz <josemaria130@outlook.es>
2025-11-24 21:26:09 -0600

    GPIO Interruption

--- project_2/GPIO/Prueba_Taller_Test/BasicNIOS_inst.v
+++ project_2/GPIO/Prueba_Taller_Test/BasicNIOS_inst.v
@@ -1,6 +1,7 @@
 	BasicNIOS u0 (
 		.clk_clk            (<connected-to-clk_clk>),            //         clk.clk
 		.leds_output_export (<connected-to-leds_output_export>), // leds_output.export
-		.reset_reset_n      (<connected-to-reset_reset_n>)       //       reset.reset_n
+		.reset_reset_n      (<connected-to-reset_reset_n>),      //       reset.reset_n
+		.gpio_input_export  (<connected-to-gpio_input_export>)   //  gpio_input.export
 	);
 
