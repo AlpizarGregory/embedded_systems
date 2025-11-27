d7bda3f66f78e26f9f5a19d9dfdba647077fa2c8
Dennis <alejimenezc@hotmail.com>
2025-11-27 16:33:01 -0600

    Update to testbench system

--- project_2/Audio_soc/audio_soc/testbench/synopsys/vcs/vcs_setup.sh
+++ project_2/Audio_soc/audio_soc/testbench/synopsys/vcs/vcs_setup.sh
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 18.1 625 win32 2025.11.15.17:20:36
+# ACDS 18.1 625 linux 2025.11.27.16:30:56
 
 # ----------------------------------------
 # vcs - auto-generated simulation script
@@ -94,12 +94,12 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 18.1 625 win32 2025.11.15.17:20:36
+# ACDS 18.1 625 linux 2025.11.27.16:30:56
 # ----------------------------------------
 # initialize variables
 TOP_LEVEL_NAME="audio_soc_tb"
 QSYS_SIMDIR="./../../"
-QUARTUS_INSTALL_DIR="C:/intelfpga_lite/18.1/quartus/"
+QUARTUS_INSTALL_DIR="/home/dennis/intelFPGA_lite/18.1/quartus/"
 SKIP_FILE_COPY=0
 SKIP_SIM=0
 USER_DEFINED_ELAB_OPTIONS=""
@@ -130,15 +130,19 @@ fi
 # ----------------------------------------
 # copy RAM/ROM files to simulation directory
 if [ $SKIP_FILE_COPY -eq 0 ]; then
-  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_ociram_default_contents.dat ./
+  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sequencer_mem.hex ./
+  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_inst_ROM.hex ./
+  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_AC_ROM.hex ./
   cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_ociram_default_contents.hex ./
-  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_ociram_default_contents.mif ./
-  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_rf_ram_a.dat ./
-  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_rf_ram_a.hex ./
   cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_rf_ram_a.mif ./
+  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_ociram_default_contents.mif ./
+  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_ociram_default_contents.dat ./
   cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_rf_ram_b.dat ./
-  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_rf_ram_b.hex ./
+  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_rf_ram_a.hex ./
   cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_rf_ram_b.mif ./
+  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_rf_ram_b.hex ./
+  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_rf_ram_a.dat ./
+  cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_SHARED_MEMORY.hex ./
   cp -f $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_RAM.hex ./
 fi
 
@@ -156,10 +160,38 @@ vcs -lca -timescale=1ps/1ps -sverilog +verilog2001ext+.v -ntb_opts dtm $ELAB_OPT
   -v $QUARTUS_INSTALL_DIR/eda/sim_lib/synopsys/cyclonev_pcie_hip_atoms_ncrypt.v \
   -v $QUARTUS_INSTALL_DIR/eda/sim_lib/cyclonev_pcie_hip_atoms.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/verbosity_pkg.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/avalon_utilities_pkg.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/avalon_mm_pkg.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_0_avalon_st_adapter_error_adapter_0.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_avalon_mm_slave_bfm.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_avalon_interrupt_sink.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_avalon_clock_source.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_avalon_reset_source.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_hps_0_hps_io_border_memory.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_hps_0_hps_io_border.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_width_adapter.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_address_alignment.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_burst_uncompressor.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_1_rsp_mux.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_arbitrator.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_1_rsp_demux.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_1_cmd_mux.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_1_cmd_demux.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_burst_adapter.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_burst_adapter_uncmpr.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_burst_adapter_13_1.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_burst_adapter_new.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_incr_burst_converter.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_wrap_burst_converter.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_default_burst_converter.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_avalon_st_pipeline_stage.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_avalon_st_pipeline_base.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_1_router_004.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_1_router_002.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_1_router.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_axi_master_ni.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_0_avalon_st_adapter.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_0_rsp_mux_001.sv \
-  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_arbitrator.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_0_rsp_mux.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_0_rsp_demux.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_0_cmd_mux_003.sv \
@@ -172,22 +204,111 @@ vcs -lca -timescale=1ps/1ps -sverilog +verilog2001ext+.v -ntb_opts dtm $ELAB_OPT
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_0_router.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_avalon_sc_fifo.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_slave_agent.sv \
-  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_burst_uncompressor.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_master_agent.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_slave_translator.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_merlin_master_translator.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_sys_sdram_pll_0_sys_pll.vo \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_hps_0_hps_io.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_phy_csr.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_avalon_st_adapter_error_adapter_0.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/alt_mem_if_ddr3_mem_model_top_ddr3_mem_if_dm_pins_en_mem_if_dqsn_en.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_altdqdqs.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_mem_if_sequencer_cpu_cv_sim_cpu_inst_test_bench.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/synopsys/hps_hmctl.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_mem_if_hps_memory_controller_top.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_rsp_mux.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_generic_ddio.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_2.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_avalon_st_adapter.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_router_003.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_cmd_mux.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_reset_sync.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/alt_mem_if_common_ddr_mem_model_ddr3_mem_if_dm_pins_en_mem_if_dqsn_en.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_mem_if_dll_cyclonev.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_avalon_mm_clock_crossing_bridge.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_scc_reg_file.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_merlin_master_agent.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_avalon_st_pipeline_base.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_cmd_xbar_demux_001.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_trk_mgr.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_reg_file.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_scc_hhp_wrapper.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_addr_router_001.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_rsp_xbar_mux.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_cmd_xbar_mux.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_id_router.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_rsp_xbar_demux.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_id_router_default_decode.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_mem_if_simple_avalon_mm_bridge.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_hhp_apb2avalon_bridge.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_merlin_arb_adder.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_addr_router_001_default_decode.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_merlin_burst_uncompressor.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_scc_hhp_phase_decode.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_addr_router.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_avalon_sc_fifo.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_scc_mgr.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_avalon_mm_bridge.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_merlin_slave_agent.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_merlin_master_translator.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_cmd_xbar_demux.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_avalon_dc_fifo.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_merlin_slave_translator.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_merlin_traffic_limiter.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_altera_merlin_arbitrator.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_hhp_decompress_avl_mm_bridge.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq/seq_addr_router_default_decode.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_acv_hard_io_pads.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_mem_if_sequencer_mem_no_ifdef_params.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_iss_probe.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altdq_dqs2_acv_connect_to_hard_phy_cyclonev.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_router_001.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_acv_hard_memphy.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_cmd_mux_001.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_pll.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_mem_if_avalon2apb_bridge.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_mem_if_hhp_qseq_top.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_acv_hard_addr_cmd_pads.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_cmd_demux.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_irq_mapper.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_router.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_mem_if_oct_cyclonev.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_mem_if_sequencer_cpu_cv_sim_cpu_inst.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_reset.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_clock_pair_generator.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_rsp_mux_001.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_p0_acv_ldc.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_cmd_demux_001.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq_lib/hmctl_synchronizer.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq_lib/alt_mem_ddrx_buffer.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/seq_lib/alt_mem_ddrx_fifo.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/hps_sdram_mm_interconnect_1_router_002.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/questa_mvc_svapi.svh \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/mgc_common_axi.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/mgc_axi_master.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/mgc_axi_slave.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_hps_0_fpga_interfaces_h2f_mpu_events.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_hps_0_fpga_interfaces.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_debug_slave_sysclk.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_debug_slave_tck.v \
-  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_debug_slave_wrapper.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_test_bench.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII_cpu_debug_slave_wrapper.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_up_avalon_reset_from_locked_signal.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_AUDIO_PLL_audio_pll.vo \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_reset_controller.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_reset_synchronizer.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_irq_mapper.sv \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_1.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_mm_interconnect_0.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_sys_sdram_pll_0.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_hps_0.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_TIMER.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_SHARED_MEMORY.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_SEVEN_SEGMENTS.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_RAM.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_NIOSII.v \
@@ -215,9 +336,8 @@ vcs -lca -timescale=1ps/1ps -sverilog +verilog2001ext+.v -ntb_opts dtm $ELAB_OPT
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_up_clock_edge.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_up_sync_fifo.v \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/audio_soc_AUDIO.v \
+  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_conduit_bfm_0005.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_conduit_bfm_0004.sv \
-  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_avalon_reset_source.sv \
-  $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_avalon_clock_source.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_conduit_bfm_0003.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_conduit_bfm_0002.sv \
   $QSYS_SIMDIR/audio_soc_tb/simulation/submodules/altera_conduit_bfm.sv \
